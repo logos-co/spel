d234224c6696ace3eab0db1eb590d14d8e7eaeea
Vaclav Pavlin <vaclav.pavlin@gmail.com>
2026-04-24 20:25:23 +0200

    fix: bump default spel-tag to v0.2.0-rc.3 and pin ruint=1.17.0 (#151)
    - Bump default spel_ref from v0.2.0-rc.1 to v0.2.0-rc.3 to fix #[lez_program] macro expansion type mismatches
    - Pin ruint = "=1.17.0" in methods/guest/Cargo.toml to avoid MSRV clash with rustc 1.88.0-dev (ruint 1.18.0 requires rustc 1.90)
    
    Fixes #140

--- spel-cli/src/init.rs
+++ spel-cli/src/init.rs
@@ -284,7 +284,7 @@ risc0-zkvm = {{ version = "=3.0.5", features = ["std"] }}
     let spel_ref = match (spel_tag, spel_rev) {
         (Some(t), _) => format!("tag = \"{}\"", t),
         (_, Some(r)) => format!("rev = \"{}\"", r),
-        _ => "tag = \"v0.2.0-rc.1\"".to_string(),
+        _ => "tag = \"v0.2.0-rc.3\"".to_string(),
     };
     // methods/guest/Cargo.toml
     write_file(root, "methods/guest/Cargo.toml", &format!(r#"[package]
@@ -305,6 +305,7 @@ risc0-zkvm = {{ version = "=3.0.5", features = ["std"] }}
 {snake_name}_core = {{ path = "../../{snake_name}_core" }}
 serde = {{ version = "1.0", features = ["derive"] }}
 borsh = "1.5"
+ruint = "=1.17.0"
 
 "#));
 
